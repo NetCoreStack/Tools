ae9eb83ff808db38902e7c9c467de441c4127d3c
Gencebay <gencebay.demir@gmail.com>
2017-05-10 16:13:21 +0300

    provide layout.cshtml without appdir option

--- src/NetCoreStack.Hisar.WebCli.Tools/Extensions/ConnectionManagerExtensions.cs
+++ src/NetCoreStack.Hisar.WebCli.Tools/Extensions/ConnectionManagerExtensions.cs
@@ -10,6 +10,16 @@ namespace NetCoreStack.Hisar.WebCli.Tools
     {
         public static async Task BroadcastAsyncFileChanged(this IConnectionManager connectionManager, string content, string fullname)
         {
+            if (string.IsNullOrEmpty(HostingHelper.MainAppDirectory))
+            {
+                // _Layout.cshtml from resources
+                fullname = PathUtility.NormalizeToWebPath(fullname);
+                await connectionManager.BroadcastBinaryAsync(Encoding.UTF8.GetBytes(content), 
+                    new RouteValueDictionary(new { mainappdir = HostingHelper.MainAppDirectory, fileupdated = fullname }));
+
+                return;
+            }
+
             fullname = fullname.Replace(HostingHelper.MainAppDirectory, string.Empty);
             fullname = PathUtility.NormalizeToWebPath(fullname);
             fullname = fullname.Replace("/wwwroot", string.Empty);
