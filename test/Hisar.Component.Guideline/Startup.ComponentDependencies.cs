8addbd71a0b6a5d7b1ce9958c7197f6cd258c7e3
Gencebay Demir <gencebay.demir@gmail.com>
2017-06-01 15:21:41 +0300

    package updates, components scripts, default layout

--- test/Hisar.Component.Guideline/Startup.ComponentDependencies.cs
+++ test/Hisar.Component.Guideline/Startup.ComponentDependencies.cs
@@ -13,8 +13,8 @@ namespace Hisar.Component.Guideline
 			["Microsoft.AspNetCore.Mvc"] = "1.1.3",
 			["Microsoft.AspNetCore.StaticFiles"] = "1.1.2",
 			["Microsoft.Extensions.Logging.Debug"] = "1.1.2",
-			["NetCoreStack.Hisar"] = "1.0.0-preview-228",
-			["NetCoreStack.Hisar.Server"] = "1.0.0-preview-228",
+			["NetCoreStack.Hisar"] = "1.0.0-preview-347",
+			["NetCoreStack.Hisar.Server"] = "1.0.0-preview-347",
 			["NetCoreStack.WebSockets.ProxyClient"] = "1.0.3"
 
         };
--- test/Hosting/Core/HostingExceptionFilter.cs
+++ test/Hosting/Core/HostingExceptionFilter.cs
@@ -1,18 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc.Filters;
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Filters;
 using NetCoreStack.Hisar;
+using NetCoreStack.Mvc.Types;
 
 namespace Hosting.Core
 {
     public class HostingExceptionFilter : IHisarExceptionFilter
     {
-        public HostingExceptionFilter()
-        {
-
-        }
-
         public void Invoke(ExceptionContext context, SystemLog systemLog)
         {
-
+            return;
         }
     }
 }
