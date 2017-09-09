54ccc03c7d9ccd64465147633933a83a4af09273
Gencebay Demir <gencebay.demir@gmail.com>
2017-09-09 21:27:08 +0300

    package updates

--- src/NetCoreStack.Hisar.WebCli.Tools/Core/CustomFileProvider.cs
+++ src/NetCoreStack.Hisar.WebCli.Tools/Core/CustomFileProvider.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.Extensions.FileProviders;
-using Microsoft.Extensions.FileProviders.Embedded;
-using Microsoft.Extensions.FileProviders.Physical;
 using Microsoft.Extensions.Primitives;
 using System;
 using System.Collections.Generic;
--- test/Hisar.Component.Guideline/Startup.ComponentDependencies.cs
+++ test/Hisar.Component.Guideline/Startup.ComponentDependencies.cs
@@ -7,15 +7,12 @@ namespace Hisar.Component.Guideline
     {
         public static IReadOnlyDictionary<string, string> ComponentDependencies { get; } = new Dictionary<string, string>
         {
-            ["AutoMapper"] = "6.0.2",
-			["AutoMapper.Extensions.Microsoft.DependencyInjection"] = "2.0.1",
-			["Microsoft.AspNetCore"] = "1.1.2",
-			["Microsoft.AspNetCore.Mvc"] = "1.1.3",
-			["Microsoft.AspNetCore.StaticFiles"] = "1.1.2",
-			["Microsoft.Extensions.Logging.Debug"] = "1.1.2",
-			["NetCoreStack.Hisar"] = "1.0.0-preview-347",
-			["NetCoreStack.Hisar.Server"] = "1.0.0-preview-347",
-			["NetCoreStack.WebSockets.ProxyClient"] = "1.0.3"
+            ["AutoMapper"] = "6.1.1",
+			["AutoMapper.Extensions.Microsoft.DependencyInjection"] = "3.0.1",
+			["Microsoft.AspNetCore.All"] = "2.0.0",
+			["NetCoreStack.Hisar"] = "2.0.0",
+			["NetCoreStack.Hisar.Server"] = "2.0.0",
+			["NetCoreStack.WebSockets.ProxyClient"] = "2.0.0"
 
         };
     }
